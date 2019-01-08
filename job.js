ca45787d12f8b2fa372c187ecc1c12e79a4295a5
aiwaya <aiwaya@gmail.com>
2019-01-08 10:53:54 +0900

    pg

--- job.js
+++ job.js
@@ -15,14 +15,14 @@ console.log('......... Start job .........');
 
 const { Client } = require('pg');
 
-/*
+
 const client = new Client({
     connectionString: process.env.DATABASE_URL,
     ssl: true
 });
 
 client.connect();
-*/
+
 
 function get_instance_log(url, callback) {
     conn.requestGet(url, function (err, res) {
