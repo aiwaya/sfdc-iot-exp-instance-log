8ea731f029f815be0a2d849719196d4f8d52c931
aiwaya <aiwaya@gmail.com>
2019-01-09 20:57:31 +0900

    g

--- database.js
+++ database.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env node
 'use strict'
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL;
 
-const client = new pg.Client(connectionString);
+const { Client } = require('pg');
+const pg_client = new Client({
+    connectionString: process.env.DATABASE_URL,
+    ssl: true
+});
+pg_client.connect();
 
-client.connect();
-client.query('CREATE TABLE instancelog(activationId character varying(20) NOT NULL,name character varying(20) NOT NULL,orchestrationId character varying(20) NOT NULL,instanceKey character varying(20) NOT NULL,createdTime timestamp NOT NULL,timestamp bigint NOT NULL,log json NOT NULL, primary key(activationId, timestamp))', (err, res) => {
+pg_client.query('CREATE TABLE instancelog(activationId character varying(20) NOT NULL,name character varying(20) NOT NULL,orchestrationId character varying(20) NOT NULL,instanceKey character varying(20) NOT NULL,createdTime timestamp NOT NULL,timestamp bigint NOT NULL,log json NOT NULL, primary key(activationId, timestamp))', (err, res) => {
     if (err) {
-        client.end();
+        pg_client.end();
         return console.error('error with PostgreSQL database', err);
     }
 });
