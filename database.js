4557511353ea06d87740ca0a2874c08149d6d4ad
aiwaya <aiwaya@gmail.com>
2019-01-10 06:38:09 +0900

    g

--- database.js
+++ database.js
@@ -1,6 +1,17 @@
 #!/usr/bin/env node
 'use strict'
 
+
+const pg = require('pg');
+const connectionString = process.env.DATABASE_URL;
+
+const client = new pg.Client(connectionString);
+client.connect();
+const query = client.query(
+    'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
+query.on('end', () => { client.end(); });
+
+/*
 const { Client } = require('pg');
 const pg_client = new Client({
     connectionString: process.env.DATABASE_URL,
@@ -16,3 +27,4 @@ pg_client.query('CREATE TABLE instancelog(activationId character varying(20) NOT
 });
 
 pg_client.end();
+    */
