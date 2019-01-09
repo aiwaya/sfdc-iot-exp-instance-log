a0aed6d1fae10c38015a812d13ef249a56293143
aiwaya <aiwaya@gmail.com>
2019-01-10 06:55:54 +0900

    g

--- database.js
+++ database.js
@@ -2,14 +2,20 @@
 'use strict'
 
 
-const pg = require('pg');
-const connectionString = process.env.DATABASE_URL;
+var pgp = require('pg-promise')(/*options*/);
+const db = pgp(process.env.DATABASE_URL);
 
-const client = new pg.Client(connectionString);
-client.connect();
-const query = client.query(
-    'CREATE TABLE items(id SERIAL PRIMARY KEY, text VARCHAR(40) not null, complete BOOLEAN)');
-query.on('end', () => { client.end(); });
+db.tx(t => {
+    return t.batch([
+        t.none('CREATE TABLE instancelog(activationId character varying(20) NOT NULL,name character varying(20) NOT NULL,orchestrationId character varying(20) NOT NULL,instanceKey character varying(20) NOT NULL,createdTime timestamp NOT NULL,timestamp bigint NOT NULL,log json NOT NULL, primary key(activationId, timestamp))')
+    ]);
+})
+    .then(data => {
+        // success;
+    })
+    .catch(error => {
+        console.log(error); // print error;
+    });
 
 /*
 const { Client } = require('pg');
