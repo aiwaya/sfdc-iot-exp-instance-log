ca07c5ba5e8c2dd4cbced872faf8755804d4145c
aiwaya <aiwaya@gmail.com>
2019-01-10 10:12:33 +0900

    g

--- job.js
+++ job.js
@@ -1,6 +1,7 @@
 #!/usr/bin/env node
 'use strict'
 
+/*
 const api_version = process.env.API_VERSION;
 const username = process.env.USERNAME;
 const password = process.env.PASSWORD;
@@ -8,8 +9,9 @@ const security_token = process.env.SECURITY_TOKEN;
 const login_url = process.env.LOGIN_URL;
 const instance_key = process.env.INSTANCE_KEY;
 const orchestration_id = process.env.ORCHESTRATION_ID;
+*/
 
-/*
+
 const api_version = '44.0'; //44.0
 const username = '20121210@demo.com';
 const password = 'abcd1234';
@@ -17,7 +19,7 @@ const security_token = '';
 const login_url = 'https://login.salesforce.com/'
 const instance_key = '1';
 const orchestration_id = '0FF10000000k9jHGAQ';
-*/
+
 
 const jsforce = require('jsforce');
 const conn = new jsforce.Connection({
@@ -59,7 +61,10 @@ function save_instance_log(url, callback) {
                     'INSERT into instancelog (activationId, name, orchestrationId, createdTimeUTC, instanceKey, log, timestamp) VALUES($1, $2, $3, $4, $5, $6, $7)',
                     [activationId, name, orchestrationId, createdTime, instanceKey, log[i], timestamp],
                     function(err, result) {
-                        if (err) {console.log(err);}
+                        if (err) {
+                            if(err.error != 'duplicate key value violates unique constraint "instancelog_pkey"')
+                                console.log(err);
+                        }
                     });
             }
             save_instance_log(res.nextPageUrl, callback);
